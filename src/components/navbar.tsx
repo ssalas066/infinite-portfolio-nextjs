2511eb2aeffdc1362675ce99937ed0677b1f5428
Sergio Salas <ssalas28@cnm.edu>
2023-05-02 15:37:52 -0600

    added mobile view button, svg icon, and handler for click event.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,6 +17,25 @@ const Navbar = () => {
                 alt=''
                 />
             </div>
+            <button
+            className='inline-flex p-3 hover:bg-gray-300 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+            onClick={handleClick}
+            >
+                <svg
+                    className='w-6 h-6'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M4 6h16M4 12h16M4 18h16'
+                            />
+                    </svg>
+            </button>
         </nav>
         
         </>
