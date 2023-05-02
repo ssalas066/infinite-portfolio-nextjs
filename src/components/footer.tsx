355a1482acf03d994c25c8088c8f8bda9a2207f0
Sergio Salas <ssalas28@cnm.edu>
2023-05-02 16:56:58 -0600

    working on fixed footer, set initial structure.

---
+++ src/components/footer.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <div className='fixed bottom-0 bg-indigo-600 text-white p-2 w-screen z-50'>
+            <ul>
+                <li className=''>
+                    <Link href={}>
+                    </Link>
+                </li>
+                <p className='text-white'>
+                    |
+                </p>
+                <li>
+                    <Link href={}>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
