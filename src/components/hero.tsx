0a00f608c9a8534f7b37554c808efc671f7fa37e
Sergio Salas <ssalas28@cnm.edu>
2023-05-03 16:40:53 -0600

    started Hero component layout. used as a const, not a function.

---
+++ src/components/hero.tsx
@@ -0,0 +1,15 @@
+// import Image
+
+const Hero = () => {
+    return (
+        <>
+        
+        <div className='container text-center py-5 mb-4'>
+        
+        </div>
+
+        </>
+    )
+}
+
+export default Hero;
