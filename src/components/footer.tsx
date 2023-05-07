feffc9aa9cf00fa9fedb9199bf24b4872628d055
Sergio Salas <ssalas28@cnm.edu>
2023-05-06 22:43:20 -0600

    placeholder social/contact email, added styling. hover not displaying for mobile view.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,18 +4,20 @@ const Footer = () => {
     return (
         <>
         <div className='fixed bottom-0 bg-indigo-600 text-white p-2 w-screen z-50'>
-            <ul>
+            <ul className='flex flex-wrap justify-center gap-6 lg:gap-12'>
                 <li className=''>
-                    <Link href={''}>
+                    <Link className='font-medium text-gray-400 no-underline hover:underline hover:text-white' href={''}>
+                        userName IG
                     </Link>
                 </li>
                 <p className='text-white'>
                     |
                 </p>
                 <li>
-                    <Link href={''}>
+                    <Link className='font-medium text-gray-400 no-underline hover:underline hover:text-white' href={''}>
+                        blah@geemail.com
                     </Link>
-                </li>f
+                </li>
             </ul>
         </div>
         </>
