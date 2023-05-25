d60080c5250e1c51a8212d78e5e3012343ade227
Sergio Salas <ssalas28@cnm.edu>
2023-05-25 15:54:13 -0600

    using lists for about section, will include skills, maybe some resume stuff.

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,23 +2,39 @@ const About = () => {
     return (
         <>
         <div className=" flex min-h-screen flex-col py-5 mb-4">
-            <div className=" -mb-32 lg:-mb-48">
-                <div className=" mt-36 mb-16 lg:mt-62">
+                <div className=" mb-10">
                     <div className=" mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
                         <div className=" grid grid-cols-1">
-                            <h1 className=" text-justify text-3xl lg:text-4xl col-span-1 mb-10 font-semibold leading-8 text-cyan-500">
+                            <h1 className=" text-justify lg:text-center text-3xl lg:text-4xl col-span-1 mb-10 font-semibold leading-8 text-cyan-500">
                                 About
                             </h1>
                         </div>
 
-                        <div className=" text-center grid grid-rows-6 lg:grid-cols-6 lg:gap-y-0 lg:gap-x-28 lg:-mb-72">
-                            <h1 className=" row-span-2 lg:col-span-2 text-2xl lg:text-4xl font-semibold font-mono tracking-tight text-black">
+                        <div className=" text-center grid grid-rows-6 lg:grid-cols-6 lg:gap-y-0 lg:gap-x-28">
+
+                            {/* <h1 className=" row-span-2 lg:col-span-2 text-xl lg:text-2xl font-semibold font-mono tracking-tight text-cyan-200">
                                 Fullstack Web Developer
-                            </h1>
+                            </h1> */}
+                            
+                            <div className=" row-span-2 lg:col-span-2 text-xl lg:text-2xl front-semibold font-mono tracking-tight text-cyan-200">
+                                <h1>Who I am</h1>
+                                <ul className="list-disc mx-10 text-justify">
+                                
+                                <li>
+                                    My interests
+                                </li>
+                                <li>
+                                    some other aspect
+                                </li>
+                                <li>
+                                    why is this not working lol
+                                </li>
+                            </ul>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
         </>
     )
