22b8e92a56bd6ba96a37d4221d502ed1c0f0a806
Sergio Salas <ssalas28@cnm.edu>
2023-06-02 16:01:21 -0600

    added row and column style. set grid col to lg, was setting cards to column structure in mobile view.

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -6,14 +6,14 @@ const Work = () => {
         <>
         <div className=" flex min-h-screen flex-col py-5 mb-4">
             <div className=" mb-10">
-                <div className=" grid grid-cols-2 mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
+                <div className=" mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
                     <div className=" grid grid-cols-1">
                         <h1 className=" text-justify lg:text-center text-3xl lg:text-4xl col-span-1 mb-10 font-semibold leading-8 text-cyan-500">
                             Recent Work
                         </h1>
                     </div>
-
-                    <div className=" col-span-1 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
+                <div className=' grid lg:grid-cols-4 grid-rows-2'>
+                    <div className=" row-span-1 lg:col-span-2 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
                         <Image className=" " src={sql} alt="snap-sql" />
                         <div className=' px-6 py-4'>
                             <div className=' font-semibold text-xl mb-2'>
@@ -26,7 +26,7 @@ const Work = () => {
                         </div>
                     </div>
 
-                    <div className=" col-span-1 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
+                    <div className=" row-span-1 lg:col-span-2 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
                         <Image className=" w-full" src={sql} alt="snap-sql" />
                         <div className=' px-6 py-4'>
                             <div className=' font-semibold text-xl mb-2'>
@@ -38,6 +38,7 @@ const Work = () => {
                             <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-medium text-gray-700 mr-2 mb-2'>#sqlchallenge</span>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
