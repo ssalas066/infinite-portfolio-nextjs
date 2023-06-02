8e4b6b9a45769063a04937fd2289f1fc0be59506
Sergio Salas <ssalas28@cnm.edu>
2023-06-02 11:39:13 -0600

    setting layout for recent work component, considering caraousel or cards used in initial pwp.

---
+++ src/components/work.tsx
@@ -0,0 +1,21 @@
+const Work = () => {
+    return (
+        <>
+        <div className=" flex min-h-screen flex-col py-5 mb-4">
+            <div className=" mb-10">
+                <div className=" mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
+                    <div className=" grid grid-cols-1">
+                        <h1 className=" text-justify lg:text-center text-3xl lg:text-4xl col-span-1 mb-10 font-semibold leading-8 text-cyan-500">
+                            Recent Work
+                        </h1>
+                    </div>
+                    
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
