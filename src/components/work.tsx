3ad7128ab5de3f44f8c866f1e4833a219e99dd02
Sergio Salas <ssalas28@cnm.edu>
2023-06-02 13:02:14 -0600

    working on making cards follow column structure, currently spaced too far apart, need more styling.

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -6,14 +6,14 @@ const Work = () => {
         <>
         <div className=" flex min-h-screen flex-col py-5 mb-4">
             <div className=" mb-10">
-                <div className=" mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
+                <div className=" grid grid-cols-2 mx-auto mt-5 mb-10 max-w-7xl px-6 lg:px-2">
                     <div className=" grid grid-cols-1">
                         <h1 className=" text-justify lg:text-center text-3xl lg:text-4xl col-span-1 mb-10 font-semibold leading-8 text-cyan-500">
                             Recent Work
                         </h1>
                     </div>
 
-                    <div className=" bg-gray-600 max-w-sm rounded overflow-hidden shadow-lg">
+                    <div className=" col-span-1 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
                         <Image className=" " src={sql} alt="snap-sql" />
                         <div className=' px-6 py-4'>
                             <div className=' font-semibold text-xl mb-2'>
@@ -26,7 +26,7 @@ const Work = () => {
                         </div>
                     </div>
 
-                    <div className=" bg-gray-600 max-w-sm rounded overflow-hidden shadow-lg">
+                    <div className=" col-span-1 bg-gray-600 max-w-sm rounded-md mb-5 overflow-hidden shadow-lg">
                         <Image className=" w-full" src={sql} alt="snap-sql" />
                         <div className=' px-6 py-4'>
                             <div className=' font-semibold text-xl mb-2'>
